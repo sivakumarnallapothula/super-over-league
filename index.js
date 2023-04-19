a01cce540a5968244a5afd9dff33d34662b3c0d3
siva <sksivaiah759@gmail.com>
2023-04-19 08:49:56 +0000

    done

---
+++ index.js
@@ -0,0 +1,16 @@
+const element = (
+  <div className="ok">
+    <h1 className="he">Super Over League</h1>
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+      className="sk"
+    />
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+      className="sk"
+    />
+    >
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
